61cc00c5801374e2d156001cc2cf5e42374a83be
Guilherme Naboa <guilhermenaboa31@gmail.com>
2021-08-24 16:30:03 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import '../styles/global.scss'
-import {CounterProvider} from '../context/counterContext'
+
 function MyApp({ Component, pageProps }) {
   return <>
 
-  <CounterProvider>
+ 
   <Component {...pageProps} />
-  </CounterProvider>
+ 
   </>
 
 }
